976f1c761341da430ea7d56d400fa10dd7974762
Shun Ueda <shu225@lehigh.edu>
2023-01-22 20:55:06 -0500

    add alphabet sorting

--- src/collection/Iterable.ts
+++ src/collection/Iterable.ts
@@ -1,24 +1,22 @@
 import { func0 } from '../util/functionTypes'
 
 export default abstract class Iterable<T> {
-  static from<T>(it: Iterable<T>): Iterable<T> {
+  static empty<T>(): Iterable<T> {
     throw new Error()
   }
-  static empty<T>(): Iterable<T> {
+  static from<T>(it: Iterable<T>): Iterable<T> {
     throw new Error()
   }
   static range<T>(start: number, end: number, step = 1): Iterable<T> {
     throw new Error()
   }
+
+  abstract [Symbol.iterator](): Iterator<T>
   abstract concat(suffix: Iterable<T>): Iterable<T>
 
+  abstract count(predicate: (T) => boolean): number
+  abstract reversed(): Iterable<T>
   abstract size(): number
 
   abstract toString(): string
-
-  abstract count(predicate: (T) => boolean): number
-
-  abstract reversed(): Iterable<T>
-
-  abstract [Symbol.iterator](): Iterator<T>
 }
--- src/collection/immutable/Seq.ts
+++ src/collection/immutable/Seq.ts
@@ -1,26 +1,22 @@
 import Iterable from '../Iterable'
 
 export class Seq<T> extends Iterable<T> {
-  toString(): string {
+  [Symbol.iterator](): Iterator<T, any, undefined> {
     throw new Error('Method not implemented.')
   }
-  [Symbol.iterator](): Iterator<T> {
-    return undefined
-  }
-
   concat(suffix: Iterable<T>): Iterable<T> {
     return undefined
   }
-
   count(predicate: (T) => boolean): number {
     return 0
   }
-
   reversed(): Iterable<T> {
     return undefined
   }
-
   size(): number {
     return 0
+  }
+  toString(): string {
+    throw new Error('Method not implemented.')
   }
 }
